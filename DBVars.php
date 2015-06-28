c519f1b4b30511f71028cdf43e3ccc0f696dfd8b
Evan <EvanCG@uw.edu>
2015-06-27 18:59:41 -0700

    Adding working getCompanyByName and API docs

---
+++ DBVars.php
@@ -0,0 +1,10 @@
+<?php
+	$gDB_host	= "localhost";
+	$gDB_name	= "angelHackDb";
+	$gDB_port	= "22";
+	$gUsername	= "root";
+	$gPassword	= "5XFWBX2Nzz";
+
+	$gDB_PDO_conn_string = 	"mysql:host=" . $gDB_host . ";" .
+							"dbname=" . $gDB_name
+?> 
