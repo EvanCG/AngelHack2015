b20539f6948019331494957cd9afc5ab1cc3ffb7
Evan <EvanCG@uw.edu>
2015-06-27 17:02:39 -0700

    Adding in basic get companies php file

---
+++ getCompanies.php
@@ -0,0 +1,30 @@
+<?php
+	include 'DBVars.php';
+	global $gDB_PDO_conn_string;
+	global $gUsername;
+	global $gPassword;
+	
+	try {
+		$conn = new PDO($gDB_PDO_conn_string, $gUsername, $gPassword);
+		$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+		//$sql = "SELECT * FROM exploreuw.TOUR";
+
+		$sql = "SELECT * FROM angelHackDb.Company;";
+		$query = $conn->prepare($sql);
+		$query->execute();
+
+		$result = $query->fetchall(PDO::FETCH_ASSOC);
+		if (count($result)) {
+			$return_array = array();
+			foreach($result as $row) {
+				array_push($return_array, $row);
+			}
+			echo json_encode($return_array);
+		} else {
+			echo json_encode();
+		}
+	} catch (PDOException $e) {
+		echo 'ERROR: ' . $e->getMessage();
+	}
+?>
